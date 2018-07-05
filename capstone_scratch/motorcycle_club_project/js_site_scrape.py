3cdb1a932b37efc2fdfd39752e0915dc8fdb15e5
Shane Cheek <unitedideas@gmail.com>
2018-07-05 14:52:15 -0700

    Isolated links to the bike and bike image links
    Looping the pages > printing to console the count, the bike year, make and model
    
    for page in pages(48):
        Scrap the page
        Save the data to the DB
    first page is https://www.dirtrider.com/Off-Road-Motorcycle-search-hub
    last pag is https://www.dirtrider.com/Off-Road-Motorcycle-search-hub?page=48

--- capstone_scratch/motorcycle_club_project/js_site_scrape.py
+++ capstone_scratch/motorcycle_club_project/js_site_scrape.py
@@ -1,4 +1,74 @@
-# Fail 1,2,3
+
+import dryscrape
+from bs4 import BeautifulSoup
+
+base_url = 'https://www.dirtrider.com'
+
+
+def full_url(sub_url):
+    return base_url + sub_url
+
+
+def has_class_but_no_id(tag):
+    return tag.has_attr('class') and not tag.has_attr('id')
+
+page_count = 2
+general_count = 1
+
+data_list = []
+
+
+def page_session(page_count):
+    print(page_count)
+    page = '?page=' + str(page_count)
+    if page_count == 0:
+        full_url = base_url + '/Off-Road-Motorcycle-search-hub'
+    else:
+        full_url = base_url + '/Off-Road-Motorcycle-search-hub' + page
+
+    sess = dryscrape.Session()
+    print('Visiting the URL...')
+    sess.set_attribute('auto_load_images', False)
+
+    sess.visit(full_url)
+
+    print('Status: ', sess.status_code())
+
+    response = sess.body()
+    soup = BeautifulSoup(response, "lxml")
+    return soup
+
+
+
+while page_count > 0:
+    soup = page_session(page_count)
+
+    result_items = soup.find_all(class_="result_item")
+    print('There are ' + str(len(result_items)) + ' items')
+
+    for item in result_items:
+        for image in item.find_all('img'):
+            print(image.get('src'))
+
+        for anchor in item.find_all('a'):
+
+            # if general_count % 0 == 0:
+            print(anchor.get('href'))
+            print('\n')
+            # print(str(anchor) + '\n')
+            print(anchor.get_text())
+            print(general_count / 2)
+            general_count += 1
+    page_count -= 1
+
+# psudo Process Code:
+    # loop over ever page
+    # look over the results on the page
+    # visit the link for that result
+
+
+
+# Failed attempts 1,2,3...
 # import sys
 # import PyQt5
 # from PyQt5.QtWidgets import QApplication
@@ -31,7 +101,7 @@
 #
 
 
-# <----------------another way of doing it ----------------->
+# <----------------Another way of doing it ----------------->
 # sess = dryscrape.Session()
 # url = 'https://www.dirtrider.com/Off-Road-Motorcycle-search-hub'
 # print ('Visiting the URL...')
@@ -47,64 +117,3 @@
 
 
 # documentation: https://www.crummy.com/software/BeautifulSoup/bs4/doc/
-import dryscrape
-from bs4 import BeautifulSoup
-
-base_url = 'https://www.dirtrider.com'
-
-
-def full_url(sub_url):
-    return base_url + sub_url
-
-
-def has_class_but_no_id(tag):
-    return tag.has_attr('class') and not tag.has_attr('id')
-
-
-general_count = 1
-
-data_list = []
-
-
-def session(page_count):
-    print(page_count)
-    page = '?page=' + str(page_count)
-    if page_count == 0:
-        full_url = base_url + '/Off-Road-Motorcycle-search-hub'
-    else:
-        full_url = base_url + '/Off-Road-Motorcycle-search-hub' + page
-
-    sess = dryscrape.Session()
-    print('Visiting the URL...')
-    sess.set_attribute('auto_load_images', False)
-
-    sess.visit(full_url)
-
-    print('Status: ', sess.status_code())
-
-    response = sess.body()
-    soup = BeautifulSoup(response, "lxml")
-    return soup
-
-page_count = 4
-
-while page_count > 0:
-    soup = session(page_count)
-
-    result_items = soup.find_all(class_="result_item")
-    print('There are ' + str(len(result_items)) + ' items')
-
-    for item in result_items:
-        for anchor in item.find_all('a'):
-            # print(anchor.get('href'))
-
-            if general_count % 2 == 0:
-                # print(str(anchor) + '\n')
-                print(anchor.get_text())
-                print(general_count / 2)
-            general_count += 1
-    page_count -= 1
-
-# loop over ever page
-# look over the results on the page
-# visit the link for that result
