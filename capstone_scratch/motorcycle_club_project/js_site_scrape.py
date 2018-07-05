fe888b38b71e777fe77f269703f636659e1be118
Shane Cheek <unitedideas@gmail.com>
2018-07-05 13:59:23 -0700

    Looping the pages > printing to console the count, the bike year, make and model
    for page in pages(48):
        Scrap the page
        Save the data to the DB
    first page is https://www.dirtrider.com/Off-Road-Motorcycle-search-hub
    last pag is https://www.dirtrider.com/Off-Road-Motorcycle-search-hub?page=48

--- capstone_scratch/motorcycle_club_project/js_site_scrape.py
+++ capstone_scratch/motorcycle_club_project/js_site_scrape.py
@@ -31,36 +31,70 @@
 #
 
 
-
-    # <!DOCTYPE html>
-    # <html>
-    # <head>
-    #   <meta charset="utf-8">
-    #   <title>Javascript scraping test</title>
-    # </head>
-    # <body>
-    #   <p id='intro-text'>No javascript support</p>
-    #   <script>
-    #      document.getElementById('intro-text').innerHTML = 'Yay! Supports javascript';
-    #   </script>
-    # </body>
-    # </html>
-
+# <----------------another way of doing it ----------------->
+# sess = dryscrape.Session()
+# url = 'https://www.dirtrider.com/Off-Road-Motorcycle-search-hub'
+# print ('Visiting the URL...')
+# # sess.set_attribute('auto_load_images', False)
+# sess.visit(url)
+# print ('Status: ', sess.status_code())
+# for link in sess.xpath("//div[@class='result_image']"):
+#     print ('Bike Link: ', link.at_xpath(".//a"))
+#
 
 
 # Gets all the items and the div contains an image src, the 'title' of the bike (it contains the year, make, model, and most of the time it has an MSRP (price)
 
-# documentation: https://www.crummy.com/software/BeautifulSoup/bs4/doc/
 
+# documentation: https://www.crummy.com/software/BeautifulSoup/bs4/doc/
 import dryscrape
 from bs4 import BeautifulSoup
-session = dryscrape.Session()
-session.visit('https://www.dirtrider.com/Off-Road-Motorcycle-search-hub')
-response = session.body()
+
+base_url = 'https://www.dirtrider.com'
+
+
+def full_url(sub_url):
+    return base_url + sub_url
+
+
+def has_class_but_no_id(tag):
+    return tag.has_attr('class') and not tag.has_attr('id')
+
+
+page_count = 47
+
+data_list = []
+
+
+# def session(page_count):
+sess = dryscrape.Session()
+print('Visiting the URL...')
+
+sess.set_attribute('auto_load_images', False)
+sess.visit(base_url + '/Off-Road-Motorcycle-search-hub')
+print('Status: ', sess.status_code())
+
+response = sess.body()
 soup = BeautifulSoup(response, "lxml")
-result_item = soup.find_all(class_="result_image")
-for link in result_item:
-    for link in soup.find_all('a'):
-        print(link.get('href'))
+result_items = soup.find_all(class_="result_item")
+print(type(result_items))
+print('There are ' + str(len(result_items)) + ' items')
 
 
+count = 1
+
+while page_count > 1:
+    for item in result_items:
+        for anchor in item.find_all('a'):
+            # print(anchor.get('href'))
+
+            if count % 2 == 0:
+                # print(str(anchor) + '\n')
+                print(anchor.get_text())
+                print(count / 2)
+            count += 1
+    page_count -= 1
+
+# loop over ever page
+# look over the results on the page
+# visit the link for that result
